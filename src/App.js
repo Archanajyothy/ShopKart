553bb2221d53e8a0e9b14558340834fc2362a912
archanajyothy <archanajyothy@gmail.com>
2023-05-09 17:15:26 +0530

    Products added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+
 function App(){
     return (
-    <div className="App">
-
-        <Router>
-            <Routes>
-                <Navbar />
-                <Route path="/" />
-                <Route path="/cart" />
-            </Routes>
-        </Router>
-
-    </div>
+        <div className="App">
+            <ShopContextProvider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Shop/>} />
+                        <Route path="/cart" element={<Cart/>} />
+                    </Routes>
+                </Router>
+            </ShopContextProvider>
+        </div>
     );
 }
+
 export default App;
